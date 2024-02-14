8ed711119a81e4241cde05c3b5937be4b2954765
josephmassgithub45 <JOSEPHMASSAQUOI45@GMAIL.COM>
2024-02-14 11:23:17 +0000

    first commit

--- hello.py
+++ hello.py
@@ -2,98 +2,25 @@ from flask import Flask,redirect,url_for,request,render_template
 
 app = Flask(__name__)
 
-#11111111111111111111111111111111111111111111111111111
 
-@app.route("/")
-def hello_world():
-		return "HELLO THIS IS FLASK"
+   
+#111111111111111111111111111111111111111111111111111111111
 
-#111111111111111111111111111111111111111111111111111111
+#PYTHON SIGN IN APP
 
-@app.route("/name")
-def hello_globe():
-		return "THE NAME IS FLASK"
-
-#111111111111111111111111111111111111111111111111111111
-
-@app.route("/test")
-def hello_test():
-		return "JUST TESTING"
-
-#111111111111111111111111111111111111111111111111111111
-
-@app.route("/greeting/<name>")
-def my_greeting(name):
-		return "Hello %s" % name
-
-#111111111111111111111111111111111111111111111111111111
-
-@app.route("/intnumber/<int:num>")
-def int_function(num):
-	return "The Int Number Entered is :%d " % num
-
-#1111111111111111111111111111111111111111111111111111111
-
-@app.route("/floatnumber/<float:num>")
-def float_function(num):
-	return "The Float Number Entered Is : %f " % num
-
-#1111111111111111111111111111111111111111111111111111111
-
-@app.route('/admin')
-def greet_admin():
-	return 'Hello Admin'
-
-@app.route('/guest/<g_name>')
-def greet_guest(g_name):
-	return 'Hello %s you are a guest.'%g_name
+@app.route('/signin_page')
+def signin_func():
+	return render_template("signin.html")
 
 
-@app.route('/user/<name>')
-def greet_user(name):
-	if name == 'admin':
-		return redirect(url_for('greet_admin'))
-	else:
-		return redirect(url_for('greet_guest',g_name=name))
+	
 
-
-#11111111111111111111111111111111111111111111111111111111
-
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
-
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
+@app.route('/signin',methods = ['POST', 'GET'])
+def signin():
    if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
-
-#11111111111111111111111111111111111111111111111111111111
-
-@app.route('/template')
-def index():
-   return '<html><body><h1 style="color:blue;">Hello world</h1></body></html>'
-
-@app.route('/hello/<user>')
-def hello_name(user):
-	return render_template("hello.html", name=user)
-
-#11111111111111111111111111111111111111111111111111111111
-
-@app.route('/login/<namein>')
-def login_func(namein):
-	return render_template("index.html",name=namein)
-
-
-@app.route('/login2')
-def login_function():
-	return render_template("index.html")	
-
-
+	   name = request.form['username']
+	   password = request.form['userpassword']
+	   return render_template('signinoutput.html',namein=name,passin=password)
 
 
 
