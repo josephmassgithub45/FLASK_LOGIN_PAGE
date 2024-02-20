e9a2023f66a670b1ce579d0b1ae5a3c0850a3baa
josephmassgithub45 <JOSEPHMASSAQUOI45@GMAIL.COM>
2024-02-20 00:08:39 +0000

    FIRST COMMIT

--- API.py
+++ API.py
@@ -17,11 +17,14 @@ def signin_func():
 
 @app.route('/signin',methods = ['POST', 'GET'])
 def signin():
-   if request.method == 'POST':
+   if request.method == 'POST' and len(request.form['username'])>0 and len(request.form['userpassword'])>=5:
 	   name = request.form['username']
 	   password = request.form['userpassword']
 	   return render_template('signinoutput.html',namein=name,passin=password)
-
+   else:
+	   return render_template('signinerror.html')
+	   
+	   
 
 
 if __name__ == "__main__":
